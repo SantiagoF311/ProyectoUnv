fa2a8d7ad0e638672f218d0df8d330930f47f678
santiagofonsecaf311 <santiagofonsecaf311@gmail.com>
2025-08-27 22:03:53 +0000

    commit

---
+++ proyecto-unv/next.config.mjs
@@ -0,0 +1,10 @@
+// next.config.mjs
+const nextConfig = {
+  experimental: {
+    allowedDevOrigins: [
+      'https://3000-firebase-proyectounvgit-1756308286504.cluster-lr6dwlc2lzbcctqhqorax5zmro.cloudworkstations.dev',
+    ],
+  },
+};
+
+export default nextConfig;
